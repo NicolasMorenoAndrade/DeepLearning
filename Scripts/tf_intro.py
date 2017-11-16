669fc3a722d8f565fb4e7b7040e187dbad28b6c3
NicolasMorenoAndrade <uselessconquest@gmail.com>
2017-11-15 21:26:22 -0500

    fixed long lines

--- Scripts/tf_intro.py
+++ Scripts/tf_intro.py
@@ -2,12 +2,15 @@
 import tensorflow as tf
 
 w = tf.Variable(0, dtype=tf.float32)
-cost = tf.add(tf.add(w**2, tf.multiply(-10., w)), 25)  # tf way to write w^20-10w + 25
+cost = tf.add(tf.add(w**2, tf.multiply(-10., w)), 25)
+# tf way to write w^2-10w + 25
 
-# the last computation of cost could also be performed with regular math syntax as long as w has been initialized as a variable
+# the last computation of cost could also be performed with regular
+# math syntax as long as w has been initialized as a variable
 # cost = w**2-10*w+25
 
-train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)  # 0.01 is the learning rate
+train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+# 0.01 is the learning rate
 
 init = tf.global_variables_initializer()
 session = tf.Session()
